75c12b9086394c19deaf03ca98e767ba77d1fb8d
Ricardo Buring <ricardo.buring@gmail.com>
2020-11-02 01:05:01 +0100

    Internalize UndirectedGraphBasis
    Use nauty (from SageMath) for graph canonicalization, generating
    graphs, and generating automorphism groups of graphs.

--- graph/undirected_graph_basis.py
+++ graph/undirected_graph_basis.py
@@ -1,19 +1,46 @@
+from util.misc import keydefaultdict
+from util.permutation import selection_sort
+from util.nauty import nauty_canonicalize, nauty_generate, nauty_has_odd_automorphism
+
 class UndirectedGraphBasis:
     """
     Basis of a module of undirected graphs.
     """
-    def __init__(self, graph_to_index, index_to_graph):
+    def __init__(self):
         """
         Initialize ``self``.
+        """
+        self._graphs = keydefaultdict(lambda key: list(filter(lambda g: not nauty_has_odd_automorphism(g), nauty_generate(*key))))
+
+    def graph_to_index(self, graph):
+        """
+        Return the index in ``self`` and the sign corresponding to the input ``graph``.
 
         INPUT:
 
-        - ``graph_to_index`` -- a function that takes an ``UndirectedGraph`` and returns the index in the basis and a sign
-
-        - ``index_to_graph`` -- a function that takes an index in the basis and returns an ``UndirectedGraph`` and a sign
+        - ``graph`` - an UndirectedGraph
         """
-        self.graph_to_index = graph_to_index
-        self.index_to_graph = index_to_graph
+        g, sign = nauty_canonicalize(graph)
+        v, e = len(g), len(g.edges())
+        try:
+            index = self._graphs[v,e].index(g)
+            return (v,e,index), sign
+        except ValueError:
+            return None, 1
+
+    def index_to_graph(self, index):
+        """
+        Return the UndirectedGraph and the sign corresponding to the index ``index`` in ``self``.
+
+        INPUT:
+
+        - ``index`` - an index in ``self``
+        """
+        v, e, idx = index
+        return self._graphs[v,e][idx], 1
 
     def __repr__(self):
+        """
+        Return a string representation of ``self``.
+        """
         return 'Basis of undirected graphs'
---
+++ util/nauty.py
@@ -0,0 +1,35 @@
+from graph.undirected_graph import UndirectedGraph
+from util.permutation import selection_sort
+from sage.graphs.graph import Graph
+from sage.graphs.graph_generators import GraphGenerators
+
+def nauty_canonicalize(g):
+    n = len(g)
+    edges = g.edges()
+    G, sigma = Graph([list(range(n)), edges]).canonical_label(certificate=True)
+    new_edges = G.edges(labels=False)
+    edge_permutation = [tuple(sorted([sigma[edge[0]],sigma[edge[1]]])) for edge in new_edges]
+    index_permutation = [edges.index(e) for e in edge_permutation]
+    return UndirectedGraph(n, list(new_edges)), selection_sort(index_permutation)
+
+def nauty_has_odd_automorphism(g):
+    n = len(g)
+    edges = g.edges()
+    G = Graph([list(range(n)), edges])
+    for sigma in G.automorphism_group().gens():
+        edge_permutation = [tuple(sorted([sigma(edge[0]),sigma(edge[1])])) for edge in edges]
+        index_permutation = [edges.index(e) for e in edge_permutation]
+        if selection_sort(index_permutation) == -1:
+            return True
+    return False
+
+def nauty_generate(num_vertices, num_edges, connected=None, biconnected=None, min_degree=0):
+    args = "{} {}:{}".format(num_vertices, num_edges, num_edges)
+    if connected:
+        args += " -c"
+    if biconnected:
+        args += " -C"
+    if min_degree != 0:
+        args += " -d{}".format(min_degree)
+    for G in GraphGenerators().nauty_geng(args):
+        yield UndirectedGraph(num_vertices, list(G.edges(labels=False)))
