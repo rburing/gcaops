672655b0ae597e2662bdf19cca46cb35070c23a0
Ricardo Buring <ricardo.buring@gmail.com>
2020-11-04 00:32:43 +0100

    Canonicalize consistently
    And don't confuse permutation with its inverse.

--- util/nauty.py
+++ util/nauty.py
@@ -7,9 +7,9 @@ def nauty_canonicalize(g):
     n = len(g)
     edges = g.edges()
     G, sigma = Graph([list(range(n)), edges]).canonical_label(certificate=True)
-    new_edges = G.edges(labels=False)
-    edge_permutation = [tuple(sorted([sigma[edge[0]],sigma[edge[1]]])) for edge in new_edges]
-    index_permutation = [edges.index(e) for e in edge_permutation]
+    new_edges = list(G.edges(labels=False))
+    edge_permutation = [tuple(sorted([sigma[edge[0]],sigma[edge[1]]])) for edge in edges]
+    index_permutation = [new_edges.index(e) for e in edge_permutation]
     return UndirectedGraph(n, list(new_edges)), selection_sort(index_permutation)
 
 def nauty_has_odd_automorphism(g):
@@ -32,4 +32,5 @@ def nauty_generate(num_vertices, num_edges, connected=None, biconnected=None, mi
     if min_degree != 0:
         args += " -d{}".format(min_degree)
     for G in GraphGenerators().nauty_geng(args):
+        G = G.canonical_label()
         yield UndirectedGraph(num_vertices, list(G.edges(labels=False)))
