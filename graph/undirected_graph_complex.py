f81f5cdefb636caeb26d003399d2ab7e29b08953
Ricardo Buring <ricardo.buring@gmail.com>
2021-03-14 19:42:05 +0100

    UndirectedGraphCochain_vector: cache differential

--- graph/undirected_graph_complex.py
+++ graph/undirected_graph_complex.py
@@ -2,6 +2,7 @@ from .undirected_graph import UndirectedGraph
 from .undirected_graph_vector_dict import UndirectedGraphVector_dict, UndirectedGraphModule_dict
 from .undirected_graph_vector_vector import UndirectedGraphVector_vector, UndirectedGraphModule_vector
 from .undirected_graph_basis import UndirectedGraphComplexBasis
+from util.misc import keydefaultdict
 from itertools import product
 
 class UndirectedGraphCochain_dict(UndirectedGraphVector_dict):
@@ -93,38 +94,44 @@ class UndirectedGraphCochain_vector(UndirectedGraphVector_vector):
         # TODO: optimize
         return sum(sum(self.homogeneous_part(v,e).insertion(i, other) for i in range(v)) for (v,e) in self.bi_gradings()) + sum((1 if e % 2 == 1 and f % 2 == 1 else -1)*sum(other.homogeneous_part(v,e).insertion(i, self.homogeneous_part(w,f)) for i in range(v)) for (v,e) in other.bi_gradings() for (w,f) in self.bi_gradings())
 
-    def differential(self):
+    def differential(self, use_cache=True):
         """
         Return the graph differential of this graph cochain.
         """
-        # TODO: cache and optimize
-        terms = []
-        for (bi_grading, vector) in self._vectors.items():
-            for (user_idx, user_coeff) in vector.items():
-                user, user_sign = self._parent._graph_basis.key_to_graph(bi_grading + (user_idx,))
-                user_coeff *= user_sign
-                vertices, edges = bi_grading
-                for position in range(vertices):
-                    # relabel user (vertices > position are shifted to make room for stick)
-                    user_edges = [[a + 1 if a > position else a, b + 1 if b > position else b] for (a,b) in user.edges()]
-                    # relabel stick
-                    stick_edges = [(position, position + 1)]
-                    # find edges which are incident to position
-                    incident = [(i,user_edges[i].index(position)) for i in range(len(user_edges)) if position in user_edges[i]]
-                    # loop over all possible new endpoints (in stick) for these edges
-                    for endpoints in product(range(2), repeat=len(incident)):
-                        # NOTE: skip creation of graphs with leaves:
-                        if endpoints.count(0) == 0 or endpoints.count(1) == 0:
-                            continue
-                        # TODO: skip handshakes, if all degrees > 2
-                        # redirect edges (which were incident to position) to stick
-                        for k in range(len(incident)):
-                            a, b = incident[k]
-                            user_edges[a][b] = position + endpoints[k]
-                        # NOTE: the convention is that stick edges go last:
-                        term = UndirectedGraph(len(user) + 1, [tuple(e) for e in user_edges] + stick_edges)
-                        terms.append([user_coeff, term])
-        return self._parent(terms)
+        if use_cache:
+            v = {}
+            for (bi_grading, vector) in self._vectors.items():
+                v[bi_grading[0] + 1, bi_grading[1] + 1] = self._parent._differentials[bi_grading] * vector
+            return self._parent.element_class(self._parent, v)
+        else:
+            # TODO: optimize
+            terms = []
+            for (bi_grading, vector) in self._vectors.items():
+                for (user_idx, user_coeff) in vector.items():
+                    user, user_sign = self._parent._graph_basis.key_to_graph(bi_grading + (user_idx,))
+                    user_coeff *= user_sign
+                    vertices, edges = bi_grading
+                    for position in range(vertices):
+                        # relabel user (vertices > position are shifted to make room for stick)
+                        user_edges = [[a + 1 if a > position else a, b + 1 if b > position else b] for (a,b) in user.edges()]
+                        # relabel stick
+                        stick_edges = [(position, position + 1)]
+                        # find edges which are incident to position
+                        incident = [(i,user_edges[i].index(position)) for i in range(len(user_edges)) if position in user_edges[i]]
+                        # loop over all possible new endpoints (in stick) for these edges
+                        for endpoints in product(range(2), repeat=len(incident)):
+                            # NOTE: skip creation of graphs with leaves:
+                            if endpoints.count(0) == 0 or endpoints.count(1) == 0:
+                                continue
+                            # TODO: skip handshakes, if all degrees > 2
+                            # redirect edges (which were incident to position) to stick
+                            for k in range(len(incident)):
+                                a, b = incident[k]
+                                user_edges[a][b] = position + endpoints[k]
+                            # NOTE: the convention is that stick edges go last:
+                            term = UndirectedGraph(len(user) + 1, [tuple(e) for e in user_edges] + stick_edges)
+                            terms.append([user_coeff, term])
+            return self._parent(terms)
 
 class UndirectedGraphComplex_vector(UndirectedGraphModule_vector):
     """
@@ -137,12 +144,27 @@ class UndirectedGraphComplex_vector(UndirectedGraphModule_vector):
         graph_basis = UndirectedGraphComplexBasis(connected=connected, biconnected=biconnected, min_degree=min_degree)
         super().__init__(base_ring, graph_basis, vector_constructor, matrix_constructor)
         self.element_class = UndirectedGraphCochain_vector
+        self._differentials = keydefaultdict(lambda key: self._differential_matrix(*key))
+        # TODO: load differentials from files
 
     def __repr__(self):
         """
         Return a string representation of this graph complex.
         """
         return 'Undirected graph complex over {} with {}'.format(self._base_ring, self._graph_basis)
+
+    def _differential_matrix(self, vertices, edges):
+        """
+        Return the graph differential restricted to bi-grading ``(vertices, edges)`` as a matrix.
+        """
+        basis = self.basis()
+        columns = basis.cardinality(vertices, edges)
+        rows = basis.cardinality(vertices + 1, edges + 1)
+        M = self._matrix_constructor(self.base_ring(), rows, columns)
+        for (idx, g) in enumerate(basis.graphs(vertices, edges)):
+            v = self(g).differential(use_cache=False).vector(vertices + 1, edges + 1)
+            M.set_column(idx, v)
+        return M
 
 def UndirectedGraphComplex(base_ring, connected=None, biconnected=None, min_degree=0, implementation='dict', vector_constructor=None, matrix_constructor=None):
     """
