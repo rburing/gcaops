1f9be4b4d6f7e6e1e4693c87fd22c7d3d4a51cbd
Ricardo Buring <ricardo.buring@gmail.com>
2021-05-16 14:12:20 +0200

    Optimize keydefaultdict
    Put most common branch first.

--- util/misc.py
+++ util/misc.py
@@ -2,8 +2,8 @@ from collections import defaultdict
 
 class keydefaultdict(defaultdict):
     def __missing__(self, key):
-        if self.default_factory is None:
-            raise KeyError(key)
-        else:
+        if self.default_factory is not None:
             ret = self[key] = self.default_factory(key)
             return ret
+        else:
+            raise KeyError(key)
