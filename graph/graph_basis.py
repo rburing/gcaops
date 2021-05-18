0adc33875cef55c2617b69661337ab387ece6e10
Ricardo Buring <ricardo.buring@gmail.com>
2021-05-18 16:34:38 +0200

    Add abstract class GraphBasis
    Generalizes UndirectedGraphBasis, which was already abstract enough.

---
+++ graph/graph_basis.py
@@ -0,0 +1,57 @@
+from abc import ABC, abstractmethod
+
+class GraphBasis(ABC):
+    """
+    Basis of a module spanned by graphs.
+
+    A basis consists of keys ``(v,e,index,...)`` where ``(v,e,index)`` identifies the isomorphism class of the graph.
+    """
+    @abstractmethod
+    def _generate_graphs(self, bi_grading):
+        """
+        Return a list of all the graphs in this basis in the given ``bi_grading``.
+        """
+        pass
+
+    @abstractmethod
+    def graphs(self, vertices, edges):
+        """
+        Return the list of graphs in this basis with the given amount of ``vertices`` and ``edges``.
+        """
+        pass
+
+    @abstractmethod
+    def cardinality(self, vertices, edges):
+        """
+        Return the number of graphs in this basis with the given amount of ``vertices`` and ``edges``.
+        """
+        pass
+
+    @abstractmethod
+    def graph_to_key(self, graph):
+        """
+        Return a tuple consisting of the key in this basis and the sign factor such that ``graph`` equals the sign times the graph identified by the key.
+
+        INPUT:
+
+        - ``graph`` -- a graph
+        """
+        pass
+
+    @abstractmethod
+    def key_to_graph(self, key):
+        """
+        Return a tuple consisting of a graph and the sign factor such that the sign times the graph equals the graph identified by the key.
+
+        INPUT:
+
+        - ``key`` -- a key in this basis
+        """
+        pass
+
+    def __repr__(self):
+        """
+        Return a string representation of this basis.
+        """
+        return 'Basis consisting of graphs'
+
--- graph/undirected_graph_basis.py
+++ graph/undirected_graph_basis.py
@@ -1,63 +1,16 @@
+from graph.graph_basis import GraphBasis
 from util.misc import keydefaultdict
 from util.permutation import selection_sort
 from util.undirected_graph_sage import undirected_graph_canonicalize, undirected_graph_generate, undirected_graph_has_odd_automorphism
-from abc import ABC, abstractmethod
 from functools import partial
 
-class UndirectedGraphBasis(ABC):
+class UndirectedGraphBasis(GraphBasis):
     """
-    Basis of a module of undirected graphs.
+    Basis of a module spanned by undirected graphs.
     
     A basis consists of keys ``(v,e,index,...)`` where ``(v,e,index)`` identifies the isomorphism class of the graph.
     """
-    @abstractmethod
-    def _generate_graphs(self, bi_grading):
-        """
-        Return a list of all the graphs in this basis in the given ``bi_grading``.
-        """
-        pass
-
-    @abstractmethod
-    def graphs(self, vertices, edges):
-        """
-        Return the list of graphs in this basis with the given amount of ``vertices`` and ``edges``.
-        """
-        pass
-
-    @abstractmethod
-    def cardinality(self, vertices, edges):
-        """
-        Return the number of graphs in this basis with the given amount of ``vertices`` and ``edges``.
-        """
-        pass
-
-    @abstractmethod
-    def graph_to_key(self, graph):
-        """
-        Return a tuple consisting of the key in this basis and the sign factor such that ``graph`` equals the sign times the graph identified by the key.
-
-        INPUT:
-
-        - ``graph`` -- an UndirectedGraph
-        """
-        pass
-
-    @abstractmethod
-    def key_to_graph(self, key):
-        """
-        Return a tuple consisting of an UndirectedGraph and the sign factor such that the sign times the graph equals the graph identified by the key.
-
-        INPUT:
-
-        - ``key`` -- a key in this basis
-        """
-        pass
-
-    def __repr__(self):
-        """
-        Return a string representation of this basis.
-        """
-        return 'Basis consisting of undirected graphs'
+    pass
 
 class UndirectedGraphComplexBasis(UndirectedGraphBasis):
     """
