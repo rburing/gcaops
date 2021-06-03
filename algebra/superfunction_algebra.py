ffef653d9da321643a312cacd732eb2f49a55a9a
Ricardo Buring <ricardo.buring@gmail.com>
2021-06-03 17:08:04 +0200

    Change Schouten bracket convention

--- algebra/superfunction_algebra.py
+++ algebra/superfunction_algebra.py
@@ -297,9 +297,9 @@ class Superfunction:
         """
         if len(self._monomial_coefficients.keys()) == 1: # first argument is homogeneous
             degree = next(iter(self._monomial_coefficients))
-            sign = 1 if degree % 2 == 0 else -1
+            sign = -1 if degree % 2 == 0 else 1
             other = self._parent(other) # handle the case of a bracket with an even function
-            return sum((self.derivative(self._parent.even_coordinate(i))*other.derivative(self._parent.gen(i)) + sign*self.derivative(self._parent.gen(i))*other.derivative(self._parent.even_coordinate(i)) for i in range(self._parent.ngens())), self._parent.zero())
+            return sum((sign*self.derivative(self._parent.gen(i))*other.derivative(self._parent.even_coordinate(i)) - self.derivative(self._parent.even_coordinate(i))*other.derivative(self._parent.gen(i)) for i in range(self._parent.ngens())), self._parent.zero())
         else:
             return sum((self.homogeneous_part(d).bracket(other) for d in self._monomial_coefficients.keys()), self._parent.zero())
 
