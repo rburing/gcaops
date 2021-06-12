a83cc5d701060b775210cd798543774f54ad71d3
Ricardo Buring <ricardo.buring@gmail.com>
2021-06-12 21:57:16 +0200

    Call nauty manually for directed graphs
    This also allows using nauty's directg.

--- util/directed_graph_sage.py
+++ util/directed_graph_sage.py
@@ -3,10 +3,31 @@ from util.permutation import selection_sort
 from itertools import combinations
 import sage.all # make SageMath work when called from Python
 from sage.graphs.digraph import DiGraph
-from sage.graphs.graph import Graph
-from sage.graphs.graph_generators import GraphGenerators
+import subprocess
+import os
 
-# TODO: use directg?
+NAUTY_PREFIX = '' # e.g. '/home/rburing/src/nauty27r1/'
+
+def nauty_generate_directed(num_vertices, num_undirected_edges, num_directed_edges, connected=None, biconnected=None, min_degree=0, loops=True):
+    geng_args = [str(num_vertices), "{}:{}".format(num_undirected_edges, num_undirected_edges)]
+    if connected:
+        geng_args.append("-c")
+    if biconnected:
+        geng_args.append("-C")
+    if min_degree != 0:
+        geng_args.append("-d{}".format(min_degree))
+    directg_args = ['-T', '-e{}'.format(num_directed_edges)]
+    if not loops:
+        directg_args.append('-o')
+    FNULL = open(os.devnull, 'w')
+    geng = subprocess.Popen((NAUTY_PREFIX + 'geng', *geng_args), stdout=subprocess.PIPE, stderr=FNULL)
+    directg = subprocess.Popen((NAUTY_PREFIX + 'directg', *directg_args), stdin=geng.stdout, stdout=subprocess.PIPE, stderr=FNULL)
+    for line in directg.stdout:
+        graph_encoding = line.decode('ascii').rstrip()
+        numbers = [int(v) for v in graph_encoding.split(' ')]
+        numbers = numbers[2:]
+        edges = [(numbers[2*i], numbers[2*i+1]) for i in range(num_directed_edges)]
+        yield DiGraph([list(range(num_vertices)), edges])
 
 def directed_graph_canonicalize(g):
     n = len(g)
@@ -32,34 +53,14 @@ def directed_graph_has_odd_automorphism(g):
     return False
 
 def directed_graph_generate(num_vertices, num_edges, connected=None, biconnected=None, min_degree=0, loops=True, has_odd_automorphism=None):
-    extra_args = ""
-    if connected:
-        extra_args += " -c"
-    if biconnected:
-        extra_args += " -C"
-    if min_degree != 0:
-        extra_args += " -d{}".format(min_degree)
-    try:
-        if loops:
-            max_loop_order = num_edges // 2 # NOTE: can have at most this many loops, while still attaining num_edges
-        else:
-            max_loop_order = 0
-        for loop_order in range(max_loop_order + 1):
-            nauty_args = "{} {}:{} {}".format(num_vertices, num_edges - loop_order, num_edges - loop_order, extra_args)
-            for G in GraphGenerators().nauty_geng(nauty_args):
-                results = []
-                for loops in combinations(G.edges(), loop_order): # NOTE: no need for triple edges, they always yield zero graphs
-                    H = Graph([G.vertices(), G.edges()], multiedges=True)
-                    H.add_edges(loops)
-                    for h in H.orientations():
-                        if h.has_multiple_edges(): # NOTE: graphs with directed multiple edges are zero
-                            continue
-                        h = h.canonical_label()
-                        g = DirectedGraph(num_vertices, list(h.edges(labels=False)))
-                        g.canonicalize_edges()
-                        if not g in results:
-                            if has_odd_automorphism is None or directed_graph_has_odd_automorphism(g) == has_odd_automorphism:
-                                yield g
-                                results.append(g)
-    except ValueError: # impossible values also errors
-        pass
+    if loops:
+        max_loop_order = num_edges // 2 # NOTE: can have at most this many loops, while still attaining num_edges
+    else:
+        max_loop_order = 0
+    for loop_order in range(max_loop_order + 1):
+        for h in nauty_generate_directed(num_vertices, num_edges - loop_order, num_edges, connected=connected, biconnected=biconnected, min_degree=min_degree, loops=loops):
+            h = h.canonical_label()
+            g = DirectedGraph(num_vertices, list(h.edges(labels=False)))
+            g.canonicalize_edges()
+            if has_odd_automorphism is None or directed_graph_has_odd_automorphism(g) == has_odd_automorphism:
+                yield g
