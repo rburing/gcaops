126248845cf4849e2b9458c1623acf7a65d83726
Ricardo Buring <ricardo.buring@gmail.com>
2021-07-02 23:55:59 +0200

    Replace memory-based cache with file-based cache
    Replace pickle with sqlite3.

--- graph/graph_cache.py
+++ graph/graph_cache.py
@@ -1,8 +1,9 @@
+from .graph_file import UndirectedGraphFileView, DirectedGraphFileView
 from abc import ABC, abstractmethod
 from util.undirected_graph_sage import undirected_graph_canonicalize, undirected_graph_generate
-from util.directed_graph_sage import directed_graph_canonicalize, directed_graph_generate
+from util.directed_graph_sage import directed_graph_canonicalize, directed_graph_generate_from_undirected, undirected_to_directed_graph_coefficient
 import os
-import pickle
+import sqlite3
 
 GRAPH_CACHE_DIR = None # e.g. '/home/rburing/src/gcaops/.graph_cache'
 
@@ -16,11 +17,11 @@ def options_to_filename(vertices, edges, directed=False, connected=False, biconn
         filename += '_bc'
     if min_degree != 0:
         filename += '_m{}'.format(min_degree)
-    if loops:
-        filename += '_loops'
+    if loops is not None:
+        filename += '_loops' if loops else '_noloops'
     if has_odd_automorphism is not None:
         filename += '_odd' if has_odd_automorphism else '_even'
-    filename += '.pickle'
+    filename += '.db'
     return filename
 
 class GraphCache(ABC):
@@ -32,65 +33,93 @@ class GraphCache(ABC):
         pass
 
     @abstractmethod
-    def graphs(self, bi_grading, **options):
-        pass
-
-    @abstractmethod
-    def _generate_graphs(self, bi_grading, **options):
+    def _add_graphs(self, result, bi_grading, **options):
         pass
 
     def graphs(self, bi_grading, **options):
-        vertices, edges = bi_grading
+        num_vertices, num_edges = bi_grading
         cache_key = bi_grading + tuple(options[k] for k in self.cache_keys)
         if cache_key in self.cache:
             return self.cache[cache_key]
         if GRAPH_CACHE_DIR is not None:
             if not os.path.isdir(GRAPH_CACHE_DIR):
                 os.makedirs(GRAPH_CACHE_DIR)
-            filename = os.path.join(GRAPH_CACHE_DIR, options_to_filename(vertices, edges, **options))
-            if os.path.isfile(filename):
-                with open(filename, 'rb') as f:
-                    my_graphs = pickle.load(f)
-            else:
-                my_graphs = list(self._generate_graphs(bi_grading, **options))
-                with open(filename, 'wb') as f:
-                    pickle.dump(my_graphs, f, pickle.HIGHEST_PROTOCOL)
+            filename = os.path.join(GRAPH_CACHE_DIR, options_to_filename(num_vertices, num_edges, **options))
+            my_graphs = self.file_view(filename, num_vertices, num_edges)
         else:
-            my_graphs = list(self._generate_graphs(bi_grading, **options))
+            my_graphs = list()
+        self._add_graphs(my_graphs, bi_grading, **options)
+        if GRAPH_CACHE_DIR is not None:
+            my_graphs.commit()
         self.cache[cache_key] = my_graphs
         return my_graphs
 
 class UndirectedGraphCache(GraphCache):
-    cache_keys = ['directed', 'connected', 'biconnected', 'min_degree', 'has_odd_automorphism']
+    cache_keys = ['connected', 'biconnected', 'min_degree', 'has_odd_automorphism']
+    file_view = UndirectedGraphFileView
 
     def canonicalize_graph(self, graph):
         return undirected_graph_canonicalize(graph)
 
-    def _generate_graphs(self, bi_grading, **options):
-        vertices, edges = bi_grading
+    def _add_graphs(self, result, bi_grading, **options):
+        num_vertices, num_edges = bi_grading
+        options['directed'] = False
         del options['directed']
-        for g in undirected_graph_generate(vertices, edges, **options):
-            yield g
+        if len(result) == 0:
+            for g in undirected_graph_generate(num_vertices, num_edges, **options):
+                result.append(g)
 
     def graphs(self, bi_grading, connected=False, biconnected=False, min_degree=0, has_odd_automorphism=True):
         options = {'directed': False, 'connected': connected, 'biconnected': biconnected, 'min_degree': min_degree, 'has_odd_automorphism': has_odd_automorphism}
         return super().graphs(bi_grading, **options)
 
 class DirectedGraphCache(GraphCache):
-    cache_keys = ['directed', 'connected', 'biconnected', 'min_degree', 'loops', 'has_odd_automorphism']
+    cache_keys = ['connected', 'biconnected', 'min_degree', 'loops', 'has_odd_automorphism']
+    file_view = DirectedGraphFileView
+
+    def __init__(self, undirected_graph_cache):
+        self._undirected_graph_cache = undirected_graph_cache
 
     def canonicalize_graph(self, graph):
         return directed_graph_canonicalize(graph)
 
-    def _generate_graphs(self, bi_grading, **options):
-        vertices, edges = bi_grading
-        del options['directed']
-        for g in directed_graph_generate(vertices, edges, **options):
-            yield g
+    def _add_graphs(self, result, bi_grading, **options):
+        num_vertices, num_edges = bi_grading
+        options['directed'] = True
+        if len(result) == 0:
+            del options['directed']
+            loops = options['loops']
+            if loops:
+                max_loop_order = num_edges // 2 # NOTE: can have at most this many loops, while still attaining num_edges
+            else:
+                max_loop_order = 0
+            del options['loops']
+
+            if GRAPH_CACHE_DIR is not None:
+                basename = options_to_filename(num_vertices, num_edges, **options)
+                orientation_filename = os.path.join(GRAPH_CACHE_DIR, 'u_to_' + basename)
+                orientation_con = sqlite3.connect(orientation_filename)
+                orientation_cur = orientation_con.cursor()
+                table_count = orientation_cur.execute('SELECT COUNT(name) FROM sqlite_master WHERE type = "table" AND name = "undirected_to_directed"')
+                if table_count.fetchone()[0] == 0:
+                    orientation_cur.execute('CREATE TABLE undirected_to_directed (undirected_graph_id INTEGER, directed_graph_id INTEGER, coefficient INTEGER)')
+                    orientation_cur.execute('CREATE INDEX index_undirected ON undirected_to_directed(undirected_graph_id)')
+                    orientation_con.commit()
+
+            for loop_order in range(max_loop_order + 1):
+                # TODO: use iterator over encodings?
+                for (g_idx, g) in enumerate(self._undirected_graph_cache.graphs((num_vertices, num_edges - loop_order), **options)):
+                    for (h_idx,h) in enumerate(directed_graph_generate_from_undirected(g, num_edges, loops=loops, has_odd_automorphism=options['has_odd_automorphism'])):
+                        result.append(h)
+                        if GRAPH_CACHE_DIR is not None and loop_order == 0: # NOTE: only graphs without loops are in the image of the orientation map
+                            c = undirected_to_directed_graph_coefficient(g, h)
+                            orientation_cur.execute('INSERT INTO undirected_to_directed (undirected_graph_id, directed_graph_id, coefficient) VALUES (?, ?, ?)', (1 + g_idx, 1 + h_idx, c))
+            if GRAPH_CACHE_DIR is not None:
+                orientation_con.commit()
 
     def graphs(self, bi_grading, connected=False, biconnected=False, min_degree=0, loops=True, has_odd_automorphism=True):
         options = {'directed': True, 'connected': connected, 'biconnected': biconnected, 'min_degree': min_degree, 'loops': loops, 'has_odd_automorphism': has_odd_automorphism}
         return super().graphs(bi_grading, **options)
 
 undirected_graph_cache = UndirectedGraphCache()
-directed_graph_cache = DirectedGraphCache()
+directed_graph_cache = DirectedGraphCache(undirected_graph_cache)
--- util/directed_graph_sage.py
+++ util/directed_graph_sage.py
@@ -1,8 +1,10 @@
 from graph.directed_graph import DirectedGraph
+from util.undirected_graph_sage import undirected_graph_to_encoding
 from util.permutation import selection_sort
 from itertools import combinations
 import sage.all # make SageMath work when called from Python
 from sage.graphs.digraph import DiGraph
+from sage.graphs.graph import Graph
 import subprocess
 import os
 
@@ -16,7 +18,7 @@ def nauty_generate_directed(num_vertices, num_undirected_edges, num_directed_edg
         geng_args.append("-C")
     if min_degree != 0:
         geng_args.append("-d{}".format(min_degree))
-    directg_args = ['-T', '-e{}'.format(num_directed_edges)]
+    directg_args = ['-T', '-e{}:{}'.format(num_directed_edges, num_directed_edges)]
     if not loops:
         directg_args.append('-o')
     FNULL = open(os.devnull, 'w')
@@ -24,6 +26,25 @@ def nauty_generate_directed(num_vertices, num_undirected_edges, num_directed_edg
     directg = subprocess.Popen((NAUTY_PREFIX + 'directg', *directg_args), stdin=geng.stdout, stdout=subprocess.PIPE, stderr=FNULL)
     for line in directg.stdout:
         graph_encoding = line.decode('ascii').rstrip()
+        numbers = [int(v) for v in graph_encoding.split(' ')]
+        numbers = numbers[2:]
+        edges = [(numbers[2*i], numbers[2*i+1]) for i in range(num_directed_edges)]
+        yield DiGraph([list(range(num_vertices)), edges])
+
+# TODO: use a single directg process for multiple inputs
+
+def nauty_generate_directed_from_undirected(undirected_graph, num_directed_edges, loops=True):
+    num_vertices = len(undirected_graph)
+    directg_args = ['-T', '-e{}:{}'.format(num_directed_edges, num_directed_edges)]
+    if not loops:
+        directg_args.append('-o')
+    FNULL = open(os.devnull, 'w')
+    directg = subprocess.Popen((NAUTY_PREFIX + 'directg', *directg_args), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=FNULL, encoding='ascii')
+    outs, errs = directg.communicate(undirected_graph_to_encoding(undirected_graph) + '\n')
+    for line in outs.split('\n'):
+        graph_encoding = line.rstrip()
+        if graph_encoding == '':
+            continue
         numbers = [int(v) for v in graph_encoding.split(' ')]
         numbers = numbers[2:]
         edges = [(numbers[2*i], numbers[2*i+1]) for i in range(num_directed_edges)]
@@ -52,18 +73,25 @@ def directed_graph_has_odd_automorphism(g):
             return True
     return False
 
-def directed_graph_generate(num_vertices, num_edges, connected=None, biconnected=None, min_degree=0, loops=True, has_odd_automorphism=None):
-    if loops:
-        max_loop_order = num_edges // 2 # NOTE: can have at most this many loops, while still attaining num_edges
-    else:
-        max_loop_order = 0
-    for loop_order in range(max_loop_order + 1):
-        for h in nauty_generate_directed(num_vertices, num_edges - loop_order, num_edges, connected=connected, biconnected=biconnected, min_degree=min_degree, loops=loops):
-            h = h.canonical_label()
-            g = DirectedGraph(num_vertices, list(h.edges(labels=False)))
-            g.canonicalize_edges()
-            if has_odd_automorphism is None or directed_graph_has_odd_automorphism(g) == has_odd_automorphism:
-                yield g
+def directed_graph_generate_from_undirected(undirected_graph, num_directed_edges, loops=True, has_odd_automorphism=None):
+    num_vertices = len(undirected_graph)
+    for h in nauty_generate_directed_from_undirected(undirected_graph, num_directed_edges, loops=loops):
+        h = h.canonical_label()
+        g = DirectedGraph(num_vertices, list(h.edges(labels=False)))
+        g.canonicalize_edges()
+        if has_odd_automorphism is None or directed_graph_has_odd_automorphism(g) == has_odd_automorphism:
+            yield g
+
+def undirected_to_directed_graph_coefficient(undirected_graph, directed_graph):
+    g = Graph(undirected_graph.edges())
+    h = Graph(directed_graph.edges())
+    are_isomorphic, sigma = g.is_isomorphic(h, certificate=True)
+    assert are_isomorphic
+    edges = directed_graph.edges()
+    edge_permutation = [edges.index((sigma[a], sigma[b])) if (sigma[a], sigma[b]) in edges else edges.index((sigma[b], sigma[a])) for (a,b) in undirected_graph.edges()]
+    sign = selection_sort(edge_permutation)
+    multiplicity = len(g.automorphism_group()) // len(DiGraph(directed_graph.edges()).automorphism_group())
+    return sign * multiplicity
 
 def directed_graph_to_encoding(g):
     n = len(g)
