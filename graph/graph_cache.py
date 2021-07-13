6d5891274b9f185546fcf51f80883ff7053c2eb8
Ricardo Buring <ricardo.buring@gmail.com>
2021-07-13 20:20:30 +0200

    Add FormalityGraphCache

--- graph/graph_cache.py
+++ graph/graph_cache.py
@@ -1,7 +1,8 @@
-from .graph_file import UndirectedGraphFileView, DirectedGraphFileView, UndirectedToDirectedGraphFileView
+from .graph_file import UndirectedGraphFileView, DirectedGraphFileView, UndirectedToDirectedGraphFileView, FormalityGraphFileView
 from abc import ABC, abstractmethod
 from util.undirected_graph_sage import undirected_graph_canonicalize, undirected_graph_generate
 from util.directed_graph_sage import directed_graph_canonicalize, directed_graph_generate_from_undirected, undirected_to_directed_graph_coefficient
+from util.formality_graph_sage import formality_graph_canonicalize, formality_graph_generate
 import os
 
 GRAPH_CACHE_DIR = None # e.g. '/home/rburing/src/gcaops/.graph_cache'
@@ -23,6 +24,29 @@ def options_to_filename(vertices, edges, directed=False, connected=False, biconn
     filename += '.db'
     return filename
 
+def formality_options_to_filename(num_ground_vertices, num_aerial_vertices, num_edges, connected=None,
+                                  max_out_degree=None, num_verts_of_max_out_degree=None, loops=None, prime=None,
+                                  has_odd_automorphism=None, positive_differential_order=None, mod_ground_permutations=False):
+    filename = 'f_{}_{}_{}'.format(num_ground_vertices, num_aerial_vertices, num_edges)
+    if connected is not None:
+        filename += '_c' if connected else '_nc'
+    if max_out_degree is not None:
+        filename += '_D{}'.format(max_out_degree)
+    if num_verts_of_max_out_degree is not None:
+        filename += '_M{}'.format(num_verts_of_max_out_degree)
+    if loops is not None:
+        filename += '_loops' if loops else '_noloops'
+    if prime is not None:
+        filename += '_prime' if prime else '_composite'
+    if has_odd_automorphism is not None:
+        filename += '_odd' if has_odd_automorphism else '_even'
+    if positive_differential_order is not None:
+        filename += '_pdo' if positive_differential_order else '_npdo'
+    if mod_ground_permutations:
+        filename += '_modground'
+    filename += '.db'
+    return filename
+
 class GraphCache(ABC):
     file_view = None.__class__
     cache_keys = []
@@ -33,7 +57,7 @@ class GraphCache(ABC):
         pass
 
     @abstractmethod
-    def graphs(self, bi_grading, **options):
+    def graphs(self, grading, **options):
         pass
 
 class UndirectedGraphCache(GraphCache):
@@ -157,5 +181,43 @@ class DirectedGraphCache(GraphCache):
                 if row[0] == undirected_graph_idx:
                     yield (row[1], row[2])
 
+class FormalityGraphCache(GraphCache):
+    cache_keys = ['connected', 'max_out_degree', 'num_verts_of_max_out_degree', 'loops', 'prime', 'has_odd_automorphism', 'positive_differential_order', 'mod_ground_permutations']
+    file_view = FormalityGraphFileView
+
+    def canonicalize_graph(self, graph):
+        return formality_graph_canonicalize(graph)
+
+    def _add_graphs(self, result, tri_grading, **options):
+        if len(result) != 0:
+            return # assume we've already done this (valid assumption because commit is done *only after* adding *all* the graphs)
+
+        num_ground_vertices, num_aerial_vertices, num_edges = tri_grading
+        for g in formality_graph_generate(num_ground_vertices, num_aerial_vertices, num_edges, **options):
+            result.append(g)
+
+    def graphs(self, tri_grading, connected=None, max_out_degree=None, num_verts_of_max_out_degree=None, loops=None, prime=None,
+               has_odd_automorphism=None, positive_differential_order=None, mod_ground_permutations=False):
+        options = {'connected': connected, 'max_out_degree' : max_out_degree, 'num_verts_of_max_out_degree' : num_verts_of_max_out_degree,
+                   'loops': loops, 'prime': prime, 'has_odd_automorphism': has_odd_automorphism, 'positive_differential_order': positive_differential_order,
+                   'mod_ground_permutations' : mod_ground_permutations}
+        num_ground_vertices, num_aerial_vertices, num_edges = tri_grading
+        cache_key = tri_grading + tuple(options[k] for k in self.cache_keys)
+        if cache_key in self.cache:
+            return self.cache[cache_key]
+        if GRAPH_CACHE_DIR is not None:
+            if not os.path.isdir(GRAPH_CACHE_DIR):
+                os.makedirs(GRAPH_CACHE_DIR)
+            filename = os.path.join(GRAPH_CACHE_DIR, formality_options_to_filename(num_ground_vertices, num_aerial_vertices, num_edges, **options))
+            my_graphs = self.file_view(filename, num_ground_vertices, num_aerial_vertices, num_edges)
+        else:
+            my_graphs = list()
+        self._add_graphs(my_graphs, tri_grading, **options)
+        if GRAPH_CACHE_DIR is not None:
+            my_graphs.commit()
+        self.cache[cache_key] = my_graphs
+        return my_graphs
+
 undirected_graph_cache = UndirectedGraphCache()
 directed_graph_cache = DirectedGraphCache(undirected_graph_cache)
+formality_graph_cache = FormalityGraphCache()
